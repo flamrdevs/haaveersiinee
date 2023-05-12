39fb1ec4824583497c099d308fddf87d2a5a2e4e
flamrdevs <flamrdevs@gmail.com>
2023-05-13 05:11:43 +0700

    chore: haaveersiinee

---
+++ src/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+import haaveersiinee from ".";
+
+describe("haaveersiinee", () => {
+  it("haaveersiinee", () => {
+    expect(haaveersiinee).toBeTypeOf("function");
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-const haaveersiinee = () => {
-  return "haaveersiinee";
-};
+import hvrsn from "hvrsn";
 
+const PI180 = Math.PI / 180;
+const rad = (deg: number) => deg * PI180;
+
+const haaveersiinee = (lat1: number, lng1: number, lat2: number, lng2: number, r?: number) =>
+  hvrsn(rad(lat1), rad(lng1), rad(lat2), rad(lng2), r);
+
+export { rad };
 export default haaveersiinee;
